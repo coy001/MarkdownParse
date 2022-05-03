2a69ca7cd0fc3db8bc9ac685ac9b6c7798e6a26c
Elvis Yu <617986265@qq.com>
2022-05-02 22:37:16 -0700

    return

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -20,33 +20,6 @@ public class MarkdownParse {
             if(openParen==-1)break;
             int closeParen = markdown.indexOf(")", openParen);
             if(closeParen==-1)break;
-
-            int bracketCheck=0;
-            int parenCheck=0;
-            while(currentIndex < markdown.length()-1){
-                int openBracketCheck=markdown.indexOf("[", closeBracket+1);
-                int closeBracketCheck=markdown.indexOf("]", closeBracket+1);
-                if(openBracketCheck>closeBracketCheck){
-                    bracketCheck=1;
-                    closeBracket=closeBracketCheck;
-                    }
-                else if(openBracketCheck==-1&&closeBracket!=-1){
-                    closeBracket=closeBracketCheck;
-                }
-                else bracketCheck=0;
-                int openParenCheck= markdown.indexOf("(", closeParen+1);
-                int closeParenCheck= markdown.indexOf(")", closeParen+1);
-                if(openParenCheck>closeParenCheck){
-                    parenCheck=1;
-                    closeParen=closeParenCheck;
-                }
-                else if(openParenCheck==-1&&closeParenCheck!=-1)
-                {
-                    closeParen=closeParenCheck;
-                }
-                else parenCheck=0;
-                if(parenCheck==0&&bracketCheck==0)break;
-            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
