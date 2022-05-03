faabc08e30463cc5a351f2a4447234ca531ebad2
Elvis Yu <617986265@qq.com>
2022-05-02 23:30:22 -0700

    change

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -58,8 +58,6 @@ public class MarkdownParse {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-        if(links.length!=0)
 	    System.out.println(links);
-        else System.out.println("It is empty");
     }
 }
