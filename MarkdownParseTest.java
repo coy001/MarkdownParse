8fa43668c0701bb2e55d5705a5308790e6683063
Elvis Yu <617986265@qq.com>
2022-05-24 05:41:03 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -9,11 +9,11 @@ import java.util.List;
 public class MarkdownParseTest {
 
     @Test
-    public void snippedOneTest throws() IOException{ 
+    public void snippedOneTest() throws IOException{ 
         Path fileName = Path.of("Snipped1.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
-        List<String> expected = List.of("url.com");
+        List<String> expected = List.of("url.com", "`google.com", "google.com", "ucsd.edu");
         assertEquals(expected, links);
     }
 
