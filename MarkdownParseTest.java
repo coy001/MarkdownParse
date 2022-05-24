27d8c5708e00a7652f106f192cc80f9fa9cf9d34
Elvis Yu <617986265@qq.com>
2022-05-24 05:28:15 -0700

    a

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -3,8 +3,28 @@ import org.junit.*;
 public class MarkdownParseTest {
 
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void snippedOneTest() throws IOException { 
+        Path fileName = Path.of("Snipped1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> expected = List.of("url.com");
+        assertEquals(expected, links);
+
+    @Test
+    public void snippedTwoTest() throws IOException{
+        Path fileName = Path.of("Snipped2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> expected = List.of("b.com");
+        assertEquals(expected, links);      
     }
 
+    @Test
+    public void snippedThreeTest() throws IOException{
+        Path fileName = Path.of("Snipped3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> expected = List.of("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        assertEquals(expected, links);      
+    }
 }
